c3a64bc78334bda159303c2e59f26b8bfd944931
Myrmex <dfusi@hotmail.com>
2022-03-02 17:34:57 +0100

    Added pg library

---
+++ projects/myrmidon/cadmus-part-itinera-pg/src/lib/cadmus-part-itinera-pg.module.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+// cadmus
+import { CadmusCoreModule, PendingChangesGuard } from '@myrmidon/cadmus-core';
+import { CadmusStateModule } from '@myrmidon/cadmus-state';
+import { CadmusUiModule } from '@myrmidon/cadmus-ui';
+import { CadmusUiPgModule } from '@myrmidon/cadmus-ui-pg';
+
+export const RouterModuleForChild = RouterModule.forChild([
+  // {
+  //   path: `${COD_BINDINGS_PART_TYPEID}/:pid`,
+  //   pathMatch: 'full',
+  //   component: CodBindingsPartFeatureComponent,
+  //   canDeactivate: [PendingChangesGuard],
+  // },
+]);
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    // Cadmus
+    RouterModuleForChild,
+    CadmusCoreModule,
+    CadmusStateModule,
+    CadmusUiModule,
+    CadmusUiPgModule,
+  ],
+  exports: [],
+})
+export class CadmusPartItineraPgModule {}
---
+++ projects/myrmidon/cadmus-part-itinera-pg/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of cadmus-part-itinera-pg
+ */
+
+export * from './lib/cadmus-part-itinera-pg.module';
