bdf721dc40fda4b700dea534c74106f1085922e3
Myrmex <dfusi@hotmail.com>
2022-05-20 17:01:56 +0200

    Poem ranges layout component

--- projects/myrmidon/cadmus-part-itinera-cod-poem-ranges/src/lib/cod-poem-ranges-layout/cod-poem-ranges-layout.component.ts
+++ projects/myrmidon/cadmus-part-itinera-cod-poem-ranges/src/lib/cod-poem-ranges-layout/cod-poem-ranges-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import {
   FormBuilder,
   FormControl,
@@ -7,6 +7,9 @@ import {
 } from '@angular/forms';
 import { PoemLayoutRow } from '../services/poem-layout-table';
 
+/**
+ * An editable poem range layout.
+ */
 @Component({
   selector: 'cadmus-cod-poem-ranges-layout',
   templateUrl: './cod-poem-ranges-layout.component.html',
@@ -24,11 +27,20 @@ export class CodPoemRangesLayoutComponent implements OnInit {
     this.updateForm(value);
   }
 
+  @Output()
+  public layoutCheck: EventEmitter<PoemLayoutRow>;
+  @Output()
+  public layoutSave: EventEmitter<PoemLayoutRow>;
+
   public editingNote?: boolean;
   public note: FormControl;
   public form: FormGroup;
 
   constructor(formBuilder: FormBuilder) {
+    // events
+    this.layoutCheck = new EventEmitter<PoemLayoutRow>();
+    this.layoutSave = new EventEmitter<PoemLayoutRow>();
+    // form
     this.note = formBuilder.control(null, Validators.maxLength(500));
     this.form = formBuilder.group({
       note: this.note,
@@ -47,13 +59,36 @@ export class CodPoemRangesLayoutComponent implements OnInit {
     this.form.markAsPristine();
   }
 
+  public setCheck(): void {
+    if (!this._layout) {
+      return;
+    }
+    this.layoutCheck.emit(this._layout);
+  }
+
+  public clearLayout(): void {
+    if (!this._layout) {
+      return;
+    }
+    this.cancelNote();
+    this._layout.checked = false;
+    this._layout.layout = undefined;
+    this._layout.note = undefined;
+    this.layoutSave.emit(this._layout);
+  }
+
+  public editNote(): void {
+    this.editingNote = true;
+  }
+
   public cancelNote(): void {
     this.editingNote = false;
   }
 
   public saveNote(): void {
-    this.editingNote = false;
+    this.cancelNote();
     const note = this.note.value?.trim();
     this._layout!.note = note ? note : undefined;
+    this.layoutSave.emit(this._layout);
   }
 }
